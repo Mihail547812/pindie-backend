dce4f13ecd5efdb2d8e5b7f0364414a76f797c83
Mihail547812 <149661426+Mihail547812@users.noreply.github.com>
2024-05-22 19:35:13 +0300

    Add files via upload

---
+++ pindie-theme-8-main/app/api/config.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://pindie-adminpindie.nomoredomainswork.ru/api' 
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/local`,
+  me: `${BASE_URL}/users/me`,
+}
