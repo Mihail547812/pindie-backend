bc054739f2fff897a7678fc5d0cb86344abed4a8
Mihail547812 <149661426+Mihail547812@users.noreply.github.com>
2024-05-22 21:13:33 +0300

    Add files via upload

---
+++ Admin Pindie/routes/categories.js
@@ -0,0 +1,50 @@
+const categoriesRouter = require('express').Router();
+
+const { findAllCategories } = require('../middlewares/categories');
+const { sendAllCategories } = require('../controllers/categories');
+const { createCategory } = require('../middlewares/categories')
+const { findCategoryById } = require('../middlewares/categories')
+const { sendCategoryById } = require('../controllers/categories')
+const { updateCategory } = require('../middlewares/categories')
+const { sendCategoryUpdated } = require('../controllers/categories')
+const { sendCategoryCreated } = require('../controllers/categories')
+const { sendCategoryDeleted } = require('../controllers/categories')
+const { deleteCategory } = require('../middlewares/categories')
+const { checkIsCategoryExists } = require('../middlewares/categories')
+const { checkEmptyName } = require('../middlewares/categories')
+const { checkAuth } = require('../middlewares/auth')
+
+
+categoriesRouter.get('/categories', findAllCategories, sendAllCategories);
+categoriesRouter.post(
+    "/categories",
+    findAllCategories,
+    createCategory,
+    sendCategoryCreated
+);
+router.get('/categories/:id', findCategoryById, sendCategoryById);
+
+categoriesRouter.post(
+  "/categories",
+  findAllCategories,
+  checkIsCategoryExists,
+  checkEmptyName,
+  checkAuth,
+  createCategory,
+  sendCategoryCreated
+);
+categoriesRouter.put(
+  "/categories/:id",
+  checkEmptyName,
+  checkAuth,
+  updateCategory,
+  sendCategoryUpdated
+);
+categoriesRouter.delete(
+  "/categories/:id",
+  checkAuth,
+  deleteCategory,
+  sendCategoryDeleted
+); 
+
+module.exports = { categoriesRouter, router }
